c777187127af618e4d1391f43e12db3d47d1615b
zml2008 <zml@aoeu.xyz>
2021-06-19 00:12:39 +0000

    Deploying to gh-pages from @ KyoriPowered/adventure-javadocs@b5f3edc3ad9c63cc36b6de2312a92741846b6751 🚀

---
+++ text-serializer-gson-legacy-impl/4.6.0/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"net.kyori.adventure.text.serializer.gson.legacyimpl","l":"NBTLegacyHoverEventSerializer"}];updateSearchResults();
---
+++ text-serializer-gson-legacy-impl/4.8.1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"net.kyori.adventure.text.serializer.gson.legacyimpl","l":"NBTLegacyHoverEventSerializer"}]
