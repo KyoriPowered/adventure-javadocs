7f6413dfa924f9687136b5e63c23693acdce7eb6
zml <zml@stellardrift.ca>
2023-07-16 11:48:10 -0700

    ansi: 1.0.2

---
+++ ansi/1.0.2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"net.kyori.ansi","l":"ANSIComponentRenderer"},{"p":"net.kyori.ansi","l":"ColorLevel"},{"p":"net.kyori.ansi","l":"StyleOps.State"},{"p":"net.kyori.ansi","l":"StyleOps"},{"p":"net.kyori.ansi","l":"ANSIComponentRenderer.ToString"},{"p":"net.kyori.ansi","l":"ANSIComponentRenderer.ToStringBuilder"}]
