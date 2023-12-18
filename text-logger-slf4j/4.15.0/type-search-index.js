a747ff4d8b6bbb2cf9d6a11909ff9825016ea62b
zml <zml@stellardrift.ca>
2023-12-17 22:55:17 -0800

    adventure: 4.15.0

---
+++ text-logger-slf4j/4.15.0/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"net.kyori.adventure.text.logger.slf4j","l":"ComponentLogger"},{"p":"net.kyori.adventure.text.logger.slf4j","l":"ComponentLoggerProvider"},{"p":"net.kyori.adventure.text.logger.slf4j","l":"ComponentLoggerProvider.LoggerHelper"}]
